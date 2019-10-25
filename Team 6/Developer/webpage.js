270446c26d784d3084d11616ffab9cd4efb5cf6b
katherinehsu1 <48663729+katherinehsu1@users.noreply.github.com>
2019-10-25 15:12:00 -0400

    Add files via upload

---
+++ Team 6/Developer/webpage.js
@@ -0,0 +1,11 @@
+function begin() {
+    var predictbutton = document.getElementById("predicbtn");
+    predictbutton.onclick = function(){predict()};
+}
+
+predict() {
+    var values = document.getElementById("textvalue");
+    return values;
+}
+
+begin();
